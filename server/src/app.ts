065c5b3233a60ca92c360f99bb410a663ea37442
James Hart <jamesmurphy197197@gmail.com>
2024-02-02 21:23:26 +0000

    [FIX] Refine port and move client to env.py

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,35 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import {users} from "../db/models/init-models";
 import { sqlconnect } from './db/postgres';
 import { mongoconnect } from './db/mongo';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
-dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.use(express.json());
 sqlconnect();
-mongoconnect().catch(console.dir);
+mongoconnect(mongoClient).catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post('/character', async (req, res) => {
+    const collection = await mongoClient.db("heroestavern").collection("characters");
+    const character = req.body;
+    collection.insertOne(character);
+    res.sendStatus(201);
+})
 
 app.get('/users', async (req, res) => {
     const allUsers = await users.findAll();
