b80615dd1552bcab62303d49825af5903c0b6099
James Hart <jamesmurphy197197@gmail.com>
2024-02-02 22:29:06 +0000

    [FEAT] Refine augment.ts to include item and options

--- server/mongo/models/augment.ts
+++ server/mongo/models/augment.ts
@@ -1,5 +1,13 @@
+import Feature from "./feature";
+import Language from "./language";
+import SavingThrowProficiency from "./savingthrowproficiency";
+import SkillProficiency from "./skillproficiency";
+import Spell from "./spell";
+import ToolProficiency from "./toolproficiency";
+
 export default interface Augment {
     name?: string,
     path: string,
-    action: "add" | "update"
+    action: "add" | "update",
+    item: Language[] | SkillProficiency[] | Spell[] | SavingThrowProficiency[] | ToolProficiency[] | Feature[]
 }
