be5128609555e0d216f225ab324d54a96598194f
James Hart <jamesmurphy197197@gmail.com>
2024-02-02 22:30:01 +0000

    [DB] Add spell seed

--- server/mongo/models/spell.ts
+++ server/mongo/models/spell.ts
@@ -1,6 +1,57 @@
+import Class from "./class"
+import Subclass from "./subclass"
+
 export default interface Spell {
-    name: string;
-    spell_level: number;
-    full_description: string;
-    spell_reqs: "None" | "Verbal" | "Somatic" | "Material" | "Verbal and Somatic" | "Somatic and Material" | "Verbal, Somatic and Material";
+  higher_level: any[]
+  index: string
+  name: string
+  desc: string[]
+  range: string
+  components: string[]
+  ritual: boolean
+  duration: string
+  concentration: boolean
+  casting_time: string
+  level: number
+  damage: Damage
+  dc: Dc
+  school: School
+  classes: Class[]
+  subclasses: Subclass[]
+  url: string
+}
+
+export interface Damage {
+  damage_type: DamageType
+  damage_at_character_level: DamageAtCharacterLevel
+}
+
+export interface DamageType {
+  index: string
+  name: string
+  url: string
+}
+
+export interface DamageAtCharacterLevel {
+  "1": string
+  "5": string
+  "11": string
+  "17": string
+}
+
+export interface Dc {
+  dc_type: DcType
+  dc_success: string
+}
+
+export interface DcType {
+  index: string
+  name: string
+  url: string
+}
+
+export interface School {
+  index: string
+  name: string
+  url: string
 }
