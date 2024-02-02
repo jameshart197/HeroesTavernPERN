f41bfd0b7902af4574c2436125b02a91e449d417
James Hart <jamesmurphy197197@gmail.com>
2024-02-02 22:28:43 +0000

    [REFAC] Format code in attribute.ts

--- server/mongo/models/attribute.ts
+++ server/mongo/models/attribute.ts
@@ -1,6 +1,12 @@
-export default interface  Attribute {
-    name: "Strength"|"Dexterity"|"Constitution"|"Intelligence"|"Wisdom"|"Charisma";
+export default interface Attribute {
+    name:
+        | { fullname: "Strength"; abbrv: "STR" }
+        | { fullname: "Dexterity"; abbrv: "DEX" }
+        | { fullname: "Constitution"; abbrv: "CON" }
+        | { fullname: "Intelligence"; abbrv: "INT" }
+        | { fullname: "Wisdom"; abbrv: "WIS" }
+        | { fullname: "Charisma"; abbrv: "CHA" };
     description: string;
     score: number;
-    modifier: ()=>number;
+    modifier: () => number;
 }
